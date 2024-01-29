53fceb8de396988020c472ca812d20c2a4a30a7e
Jonathan Lao <jon.lao@gmail.com>
2024-01-29 13:50:21 -0600

    Implement Feature Admin Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'about', to: 'about#index'
   root to: 'products#index'
+  get 'about', to: 'about#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
+    resources :categories, only: [:index, :new, :create]
     resources :products, except: [:edit, :update, :show]
   end
 
